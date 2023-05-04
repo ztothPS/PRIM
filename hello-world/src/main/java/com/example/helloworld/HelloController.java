afee77c8a7400414774893018566f5176f462d3a
ztoth <zoltan.toth.2012@gmail.com>
2023-05-04 13:13:38 +0200

    Init

---
+++ hello-world/src/main/java/com/example/helloworld/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greetings(){
+        return "It's done";
+    }
+
+}
